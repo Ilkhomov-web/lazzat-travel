64324bdfce60ff4e3665ca5310e0e123fe4b80f6
ilkhomov-web <ilhomovelyor119@gmail.com>
2025-07-17 16:42:36 +0500

    some change

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,60 @@
+import {
+  Box,
+  Button,
+  Container,
+  ListItem,
+  ListItemButton,
+} from "@mui/material";
+import React from "react";
+import TranslateButton from "./translateButton";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import { useTranslation } from "react-i18next";
+
+function Navbar() {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <Box sx={{ width: "100%", background: "#0056b8" }}>
+      <Container maxWidth={"xl"}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Box component={"img"} src="/logo.png" width={"150px"}></Box>
+          <ListItem sx={{ width: "40%", color: "white" }}>
+            <ListItemButton>{t("home")}</ListItemButton>
+            <ListItemButton>{t("about")}</ListItemButton>
+            <ListItemButton>{t("tour")}</ListItemButton>
+            <ListItemButton>{t("contact")}</ListItemButton>
+          </ListItem>
+
+          <Box
+            sx={{
+              width: "30%",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Button>
+              <InstagramIcon sx={{ color: "white", fontSize: "28px" }} />
+            </Button>
+            <Button>
+              <TelegramIcon sx={{ color: "white", fontSize: "28px" }} />
+            </Button>
+            <Button>
+              <InstagramIcon sx={{ color: "white", fontSize: "28px" }} />
+            </Button>
+            <TranslateButton />
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
+}
+
+export default Navbar;
---
+++ src/components/translateButton.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import Popover from "@mui/material/Popover";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { useTranslation } from "react-i18next";
+
+const languages = [
+  { code: "uz", label: "Uz", flag: "uz-flag.png" },
+  { code: "ru", label: "Ru", flag: "rus-flag.png" },
+  { code: "en", label: "En", flag: "usa-flag.png" },
+];
+
+function TranslateButton() {
+  const { i18n } = useTranslation();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLanguageChange = (lng) => {
+    i18n.changeLanguage(lng);
+    handleClose();
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "language-popover" : undefined;
+
+  const langCode = i18n.language.split("-")[0];
+  const currentLang =
+    languages.find((lang) => lang.code === langCode) || languages[0];
+
+  return (
+    <div>
+      <Button
+        sx={{ background: "white" }}
+        aria-describedby={id}
+        variant="outlined"
+        onClick={handleClick}
+        startIcon={
+          <img
+            src={currentLang.flag}
+            alt={currentLang.code}
+            style={{ width: 20, height: 14, objectFit: "cover" }}
+          />
+        }
+      >
+        {currentLang.label}
+      </Button>
+
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+      >
+        <List sx={{ minWidth: 120 }}>
+          {languages.map((lang) => (
+            <ListItem disablePadding key={lang.code}>
+              <ListItemButton onClick={() => handleLanguageChange(lang.code)}>
+                <ListItemIcon>
+                  <img
+                    src={lang.flag}
+                    alt={lang.code}
+                    style={{ width: 20, height: 14, objectFit: "cover" }}
+                  />
+                </ListItemIcon>
+                <ListItemText primary={lang.label} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Popover>
+    </div>
+  );
+}
+
+export default TranslateButton;
